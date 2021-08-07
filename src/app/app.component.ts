d65a487dc3e4e55ce3433c7948ec5f4ff407e363
whatsmysuggestion <whatsmysuggestion@gmail.com>
2021-08-07 22:52:17 +0530

    ngFor-Directive-Using-Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+	
+	
+  List: any[] = [1,2,3,4,5,6,7,8];	
+  
+}
